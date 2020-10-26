9fee99921a777e4761ebee561d37c649484d87da
Alexander Peskov <alexander.peskov@intel.com>
2020-10-26 19:49:29 +0300

    [FIX] Add several uni instruction wrappers into jit_generator
    Remove redundancy of two unified instructions
    
    3.5 squash below:
    [FIX] Add uni_vhsubps and uni_vaddsubps into jit_generator (#142)
    
    * Add uni_vhsubps and uni_vaddsubps into jit_generator.hpp
    
    * Add missing namespace
    
    * Adjust indentation and add braces
    
    * Improve formatting
    
    * Replace assertion in uni_vhsubps and uni_vhaddps with possible workaround for SSE 4.1
    
    [FIX] added uni_vcvttps2dq

--- src/cpu/x64/jit_generator.hpp
+++ src/cpu/x64/jit_generator.hpp
@@ -767,6 +767,18 @@ public:
         }
     }
 
+    void uni_vhsubps(const Xbyak::Xmm &x, const Xbyak::Xmm &x2,
+                     const Xbyak::Operand &op) {
+        if (is_valid_isa(avx)) {
+            vhsubps(x, x2, op);
+        } else {
+            if (!x.isEqualIfNotInherited(x2)) {
+                movups(x, x2);
+            }
+            hsubps(x, op);
+        }
+    }
+
     void uni_vpsignd(const Xbyak::Xmm &x1, const Xbyak::Xmm &x2,
             const Xbyak::Operand &op) {
         if (is_valid_isa(avx))
@@ -865,6 +877,18 @@ public:
     void uni_vsubps(const Xbyak::Ymm &x, const Xbyak::Operand &op1,
             const Xbyak::Operand &op2, const Xbyak::Ymm &buf) {
         vsubps(x, op1, op2);
+    }
+
+    void uni_vaddsubps(const Xbyak::Xmm &x, const Xbyak::Operand &op1,
+                       const Xbyak::Operand &op2) {
+        if (is_valid_isa(avx)) {
+            vaddsubps(x, op1, op2);
+        } else {
+            if (!x.isEqualIfNotInherited(op1)) {
+                movups(x, op1);
+            }
+            addsubps(x, op2);
+        }
     }
 
     void uni_vpmulld(const Xbyak::Xmm &x1, const Xbyak::Xmm &x2,
@@ -1525,6 +1549,16 @@ public:
         vcmpps(x1, x2, op, cmp_predicate);
     }
 
+    void uni_cmpneqps(const Xbyak::Xmm &x1, const Xbyak::Xmm &x2,
+            const Xbyak::Operand &op) {
+        if (is_valid_isa(avx))
+            vcmpneqps(x1, x2, op);
+        else {
+            if (x1.getIdx() != x2.getIdx()) uni_vmovups(x1, x2);
+            cmpneqps(x1, op);
+        }
+    }
+
     void uni_vtestps(const Xbyak::Xmm &x1, const Xbyak::Operand &op) {
         if (is_valid_isa(avx))
             vtestps(x1, op);
@@ -1635,6 +1669,17 @@ public:
             vcvtps2phx(x1, x2);
         else if (is_valid_isa(avx2))
             vcvtps2ph(x1, x2, _op_mxcsr);
+    }
+
+    void uni_vcvttps2dq(const Xbyak::Xmm &x, const Xbyak::Operand &op) {
+        if (is_valid_isa(avx))
+            vcvttps2dq(x, op);
+        else
+            cvttps2dq(x, op);
+    }
+
+    void uni_vcvttps2dq(const Xbyak::Ymm &x, const Xbyak::Operand &op) {
+        vcvttps2dq(x, op);
     }
 
     void uni_vmovmskps(const Xbyak::Reg &x1, const Xbyak::Xmm &x2) {
@@ -1995,6 +2040,10 @@ public:
         }
     }
 
+    void uni_vpminsd(const Xbyak::Ymm &x1, const Xbyak::Ymm &x2, const Xbyak::Operand &op) {
+        vpminsd(x1, x2, op);
+    }
+
     void uni_movshdup(const Xbyak::Xmm &x, const Xbyak::Operand &op) {
         if (is_valid_isa(avx))
             vmovshdup(x, op);
@@ -2020,6 +2069,17 @@ public:
     }
 
     // End of custom instructions section.
+
+    void uni_vcmpgtps(const Xbyak::Xmm &x1, const Xbyak::Xmm &x2,
+                      const Xbyak::Operand &op) {
+        assert(x1.getIdx() == x2.getIdx());
+        cmpps(x1, op, _cmp_nle_us);
+    }
+
+    void uni_vcmpgtps(const Xbyak::Ymm &x1, const Xbyak::Ymm &x2,
+                      const Xbyak::Operand &op) {
+        vcmpgtps(x1, x2, op);
+    }
 
     void mul_by_const(
             const Xbyak::Reg &out, const Xbyak::Reg64 &tmp, int value) {
