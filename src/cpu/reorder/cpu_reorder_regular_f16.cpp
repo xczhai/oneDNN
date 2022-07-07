9f405714dace78f22df95065f1f36c7a8c912005
zihan wu <zihan.wu@intel.com>
2022-07-07 11:06:02 +0800

    [FORK][FEATURE] Cc based on master (#135)
    * select build v26
    
    * fix constexpr in cc onednn v26
    
    * fix bug on test model yolov3
    
    * clean code deleta unused constexpr version
    
    * ONEDNN 3.2 migration squashed commits:
       - Fix Windows+CPU Plugin+CC compile fail issue.
       - Remove f16 kernel instances
       - remove unused primitive (#127)
    
    Add FP16 kernel back.
    Fix missing FP16 reorder ref kernel.
    
    [FORK][FIX] Fix CC caused compiling problem when merging 3.5
    [FORK][FEATURE] Cc based on master (#135)
    
    ONEDNN 3.5 squash list:
    [FORK][UPDATE] use ov_mark_target_as_cc instead of deprecated ie_mark_target_as_cc
    
    [ARM] Fixed missed using stmt for ARM target

--- src/cpu/reorder/cpu_reorder_regular_f16.cpp
+++ src/cpu/reorder/cpu_reorder_regular_f16.cpp
@@ -29,15 +29,15 @@ const impl_list_map_t &regular_f16_impl_list_map() {
             DNNL_AARCH64_ONLY(REG_SR_DIRECT_COPY(f16, f16))
 
             DNNL_X64_ONLY(CPU_REORDER_INSTANCE(x64::brgemm_matmul_matrix_B_reorder_t))
-            DNNL_X64_ONLY(CPU_REORDER_INSTANCE(x64::jit_blk_reorder_t))
-            DNNL_X64_ONLY(CPU_REORDER_INSTANCE(x64::jit_uni_reorder_t))
+            DNNL_X64_ONLY(CPU_REORDER_INSTANCE(x64_jit_blk_reorder_t))
+            DNNL_X64_ONLY(CPU_REORDER_INSTANCE(x64_jit_uni_reorder_t))
 
-            REG_SR(f16, any, f8_e5m2, any, fmt_order::any, spec::reference)
-            REG_SR(f16, any, f8_e4m3, any, fmt_order::any, spec::reference)
-            REG_SR(f16, any, f16, any, fmt_order::any, spec::reference)
-            REG_SR(f16, any, f32, any, fmt_order::any, spec::reference)
-            REG_SR(f16, any, s8, any, fmt_order::any, spec::reference)
-            REG_SR(f16, any, u8, any, fmt_order::any, spec::reference)
+            REG_SR(f16, any, f8_e5m2, any, fmt_order_any, spec_reference)
+            REG_SR(f16, any, f8_e4m3, any, fmt_order_any, spec_reference)
+            REG_SR(f16, any, f16, any, fmt_order_any, spec_reference)
+            REG_SR(f16, any, f32, any, fmt_order_any, spec_reference)
+            REG_SR(f16, any, s8, any, fmt_order_any, spec_reference)
+            REG_SR(f16, any, u8, any, fmt_order_any, spec_reference)
 
             nullptr,
         }},
